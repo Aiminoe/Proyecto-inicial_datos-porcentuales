d7681eed864f81fef73f6768fb938427d1a2f950
Aiminoe <106620686+Aiminoe@users.noreply.github.com>
2022-09-07 02:07:00 -0300

    modificado

--- Funciones.py
+++ Funciones.py
@@ -12,7 +12,7 @@ def archivo_csv():
 
 #funcion para extender la lista de paises de origen y dar entrada a la seleccion del usuario
     
-def origen_1(opcion_1):
+def origen_1():
         paises = {"1": "Brasil" , "2": "Chile", "3": "Bolivia" , "4": "Uruguay", "5": "Paraguay", "6": "Europa", "7": "Resto del mundo", "8": "EE.UU y Canada", "9": "Resto America", "10": "FIN"}   
         print('''Ingrese el numero del pais de residencia que desea selecionar, o el numero para finalizar: 
             1)Brasil
@@ -25,14 +25,14 @@ def origen_1(opcion_1):
             8)EE.UU y Canada
             9)Resto America
             10)FIN''')
-        
+        opcion_1 = input("Ingrese una opcion: ")
         print()
         residencia = paises.get(opcion_1, '')
         return residencia
 
 #funcion para extender la lista de provincias de destino y dar entrada ala seleccion del usuario   
 
-def destino_1(opcion_2):
+def destino_1():
     provincias = {"1": "Buenos Aires", "2": "Caba", "3": "Cordoba", "4": "Catamarca", "5": "Chaco", "6": "Chubut", "7": "Corrientes", "8": "Entre Rios", "9": "Formosa", "10": "Jujuy", "11": "La Pampa", "12": "Formosa", "13": "La Rioja", "14": "Mendoza", "15": "Misiones", "16": "Neuquen", "17": "Rio Negro", "18": "Salta", "19": "San Juan", "20": "San Luis", "21": "Santa Cruz", "22": "Santa Fe", "23": "Santiago del Estero", "24": "Tierra del Fuego", "25": "Tucuman"}
     print(''' Selecione el numero de la provincia de destino
             1)Buenos Aires
@@ -61,7 +61,7 @@ def destino_1(opcion_2):
             24)Tierra del Fuego
             25)Tucuman
             ''')
-
+    opcion_2 = input("Ingrese una opcion: ")
     destino = provincias.get(opcion_2,'') 
     print()
     return destino
@@ -158,15 +158,15 @@ if __name__ == '__main__':
     
     while opcion_1 != '10':
 
-        lugar_origen = origen_1(opcion_1)
-        opcion_1 = input("Ingrese una opcion: ")
+        lugar_origen = origen_1()
+        
 
         if opcion_1.isdigit():
 
             if opcion_1 != '10':
 
-                provincia_destino = destino_1(opcion_2)
-                opcion_2 = input("Ingrese una opcion: ")
+                provincia_destino = destino_1()
+                
 
                 if opcion_2.isdigit():
                     
